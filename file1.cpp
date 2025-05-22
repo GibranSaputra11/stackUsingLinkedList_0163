50346c63ebd997a820fa28e4611cd2032a1e98e8
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-22 10:10:25 +0700

    add member pop

--- file1.cpp
+++ file1.cpp
@@ -32,6 +32,15 @@ class Stack{
         return value;
     }
 
-    
+    // Pop opreation: Remove the topmost element from the stack
+    void pop(){
+        if (isEmpty()){
+            cout << "Stack is empty." << endl;
+        }
+
+        Node* temp = top; // Create a temporary pointer to the top node
+        top = top->next; // Update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+    }
 
 };
