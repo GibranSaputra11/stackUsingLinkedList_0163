f29baa71b92e8f0aa6e4b10702ec0c1333ac389c
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-22 10:00:37 +0700

    add member public

--- file1.cpp
+++ file1.cpp
@@ -17,6 +17,18 @@ class Stack{
     private:
         Node* top; //Pointer to the top node of the stack
 
+    public:
+        Stack(){
+            top = NULL; //Initialize the stack with a null top pointer
+        }
     
+    // Push operator: Insert an element onto the top of the stack
+    int push(int value){
+        Node* newNode = new Node(); // 1. Alocate memory for the new node
+        newNode->data = value; // 2. Assign value
+        newNode->next = top; //3. Set the next pointer of the new node to the current top
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push Value: " << value << endl;
+    }
 
 };
