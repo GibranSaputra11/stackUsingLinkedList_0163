5adcece60208942d4a929e9da0485ad29a90ae22
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-22 10:28:42 +0700

    add case 1

--- file1.cpp
+++ file1.cpp
@@ -79,7 +79,11 @@ int main(){
         cin >> choice;
 
         switch(choice){
-            
+            case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); // Push the enterd value onto the stack
+            break;
         }
     }
 }
